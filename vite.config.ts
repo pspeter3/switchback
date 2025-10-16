c223edda7b11a969f1067f73e93ad08ad2cb013d
Phips Peter <phips@asana.com>
2025-10-16 10:34:30 -0400

    Add coverage

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from "vite";
+import { coverageConfigDefaults } from "vitest/config";
 
 export default defineConfig({
   test: {
     environment: "happy-dom",
     globals: true,
+    coverage: {
+      enabled: true,
+      exclude: [...coverageConfigDefaults.exclude, "src/main.tsx"],
+      thresholds: {
+        statements: 100,
+        branches: 100,
+        functions: 100,
+        lines: 100,
+      },
+    },
   },
 });
